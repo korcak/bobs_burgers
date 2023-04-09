0a3f4549190681ae22d6f3824ceab94444b0f3d4
korcak <109915099+korcak@users.noreply.github.com>
2023-04-09 13:31:03 -0400

    Initial commit using Java, array listview.

---
+++ app/src/main/java/com/example/bobsburgers/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.bobsburgers;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+    CardView dessertsCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
+        dessertsCard = findViewById(R.id.card_view_desserts);
+
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+                startActivity(startersActivityIntent);
+            }
+        });
+
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+
+                startActivity(mainCoursesActivityIntent);
+            }
+        });
+
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent dessertsActivityIntent = new Intent(MainActivity.this, DessertsActivity.class);
+
+                startActivity(dessertsActivityIntent);
+            }
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:bob@bobsburgers.com"));
+                startActivity(launchEmailAppIntent);
+            }
+        });
+    }
+}
